8e117dc0f75a6a5e5ad52adc46882326c559c0bc
Mark Evans <mark.evans@teamsnap.com>
2015-12-31 10:04:28 -0700

    tinkering with SSL settings for heroku custom domain

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :markapp_phx, MarkappPhx.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "calm-badlands-9587.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: "calm-badlands-9587.herokuapp.com", port: 443],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
