5ae73c213c75fb8ee168ce58fb1173df8076642c
Mark Evans <mark.evans@teamsnap.com>
2015-12-30 19:03:41 -0700

    oops

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :markapp_phx, MarkappPhx.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "calm-badlands-9587.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :hello_phoenix, HelloPhoenix.Repo,
